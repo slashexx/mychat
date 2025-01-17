f724ebdb94ccd10f6849afda115eb4d159aee91b
slashexx <dhruvpuri.35@gmail.com>
2025-01-17 18:42:12 +0530

    specify port
    Signed-off-by: slashexx <dhruvpuri.35@gmail.com>

--- model.py
+++ model.py
@@ -69,4 +69,4 @@ def chat():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
